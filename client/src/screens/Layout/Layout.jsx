70d9eef7e4f8c78852e616913d7651e10395abc0
juan321654 <JuanCamachopers@hotmail.com>
2020-10-20 13:59:24 -0400

    layout with nav header no links yet

---
+++ client/src/screens/Layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Layout.css';
+import { Link } from 'react-router-dom'
+
+export default function Layout() {
+    return (
+        <div className="main-lay">
+            <Link to="/">
+                GorJeo
+            </Link>
+
+            <div className="li-l">
+                <li className="single-li-lay">New Post</li>
+                <li className="single-li-lay">Sign in</li>
+                <li className="single-li-lay">Log out</li>
+            </div>
+        </div>
+    )
+}
