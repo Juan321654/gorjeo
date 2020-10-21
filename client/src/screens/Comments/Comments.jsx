c71b95a3427689e38da8c2d2f3e5981fe14a2d7f
juan321654 <JuanCamachopers@hotmail.com>
2020-10-21 11:14:27 -0400

    comment component is done not functional

--- client/src/screens/Comments/Comments.jsx
+++ client/src/screens/Comments/Comments.jsx
@@ -3,9 +3,16 @@ import './Comments.css'
 
 export default function Comments() {
     return (
-        <div>
-            <img src="https://www.pngkit.com/png/detail/281-2812821_user-account-management-logo-user-icon-png.png" width="50px" height="40px" />
-            comments
+        <div className="main-con-comm">
+            <div className="header-comm">
+                <img src="https://www.pngkit.com/png/detail/281-2812821_user-account-management-logo-user-icon-png.png" width="50px" height="40px" />
+                <p className="p-comm">Juan Camacho</p>
+                <p className="p-comm">Software Engineer</p>
+            </div>
+            <hr className="hr-comm" />
+            <div className="comment-comm">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi laoreet purus id ipsum viverra, non hendrerit massa dapibus. Donec ipsum elit, porttitor a pellentesque non, pharetra eget est. Maecenas faucibus tincidunt sapien, id sollicitudin nisl feugiat sed.
+            </div>
         </div>
     )
 }
