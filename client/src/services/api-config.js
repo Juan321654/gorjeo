01d951f5f9a985b4d9f18c100e9d996a42cad8b0
juan321654 <JuanCamachopers@hotmail.com>
2020-10-24 16:34:55 -0400

    url change

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseUrl = 'http://localhost:3000'
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://git.heroku.com/gorjeo-api.git' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://gorjeo-api.herokuapp.com/' : 'http://localhost:3000'
 
 // https://git.heroku.com/gorjeo-api.git
 const api = axios.create({
